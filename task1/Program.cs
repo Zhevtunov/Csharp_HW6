467bc24b8d8848432a487069e40e6efcd5ca6fba
Alexandr <A_L_E_X1987@list.ru>
2023-02-09 11:13:16 +0300

    задачи

---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveNum(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] > 0) count += 1;
+  }
+  return count;
+}
+
+int[] GetArr(int num)
+{
+  int[] arr = new int[num];
+  for (int i = 0; i < num; i++)
+  {
+    arr[i] = GetNum($"Введи {i + 1} - ое число : ");
+  }
+  return arr;
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int number = GetNum(" Сколько чисел вы хотите ввести? : ");
+int[] array = GetArr(number);
+Console.Write($"[{String.Join(", ", array)}] чисел больше 0 -> ");
+int posNum = PositiveNum(array);
+Console.WriteLine(posNum);
